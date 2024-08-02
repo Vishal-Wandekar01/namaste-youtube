5a070e2fdcd1c00bc7a6f49f9042d634884e549a
Vishal Wandekar <varunwan@tibco.com>
2024-08-02 10:05:22 +0530

    installed redux toolkit and react redux, created our store and created slice, provide our store to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import Body from "./components/Body";
 import Head from "./components/Head";
+import store from "./Utils/store";
 
 function App() {
   return (
+<Provider store={store}>
+
     <div>
       
       <Head />
       <Body />
     </div>
+
+    </Provider>
   );
 }
 
