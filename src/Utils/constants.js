a5192b073370fe475597f990862074bd25bcecb5
Vishal Wandekar <varunwan@tibco.com>
2024-08-13 10:10:23 +0530

    Fetch data from live youtube api, use that data to display on screen, created seperate VideoCard.js to display data, iterate data through map

---
+++ src/Utils/constants.js
@@ -0,0 +1,8 @@
+
+export const API_KEY = "AIzaSyBKbVbQVncsrIvw40ShgZ5D4MjPDjqfpgA";
+// youtube api key auth > Credentials page > Create Credential > API key
+
+export const YOUTUBE_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=' + API_KEY ;
+
+// youtube apis video
+
