46b8f16c09d7bcc992b335f66a46138463915b3b
Vishal Wandekar <varunwan@tibco.com>
2024-08-15 19:28:12 +0530

    Created watch page and now we can watch each video display on screen

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,23 +1,38 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../utils/appSlice';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 const WatchPage = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  console.log(searchParams.get("v"));
 
 
   const dispatch = useDispatch();
 
-
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(closeMenu());
-  },[])
-
+  }, []);
 
   return (
-    <div>
-   <h1> WatchPage</h1>
+    <div className='px-5 '>
+      <h1>
+        {" "}
+        <iframe
+          width="1200"
+          height="600"
+          src={"https://www.youtube.com/embed/"+ searchParams.get("v")}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </h1>
     </div>
-  )
-}
+  );
+};
 
 export default WatchPage
