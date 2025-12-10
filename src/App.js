a381cc2bff8b9b575391a82a63f621d1294d4734
Vishal Wandekar <vishalwandekar@gmail.com>
2025-12-10 17:02:58 +0530

    Created Head Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 
 import './App.css';
+import Head from './components/Head';
 import './index.css';
 
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-10">
-      This is a Tailwind CSS styled component.
+    <div >
+      <h1 className="text-3xl font bold">Namaste react</h1>
+      <Head />
     </div>
   );
 }
 
 export default App;
+
+
+/**
+ * Head
+ * Body
+ *  Sidebar
+ *    MenuItems
+ *  MainContainer
+ *    ButtonList
+ *    VideoContainer
+ *      VideoCard
+ */
