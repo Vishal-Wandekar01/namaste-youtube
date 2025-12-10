90d2762b2827307a2aceed2d244552d93131551a
Vishal Wandekar <vishalwandekar@gmail.com>
2025-12-10 16:52:16 +0530

    setup Tailwind css

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // Tailwind CSS plugin
+    autoprefixer: {},           // Autoprefixer plugin
+  },
+};
+// npm install --save-dev postcss
+//npm install postcss-loader
+//npm start
