87b6492557c6f4b1efefdbb1ac02fe8f4f89083c
Vishal Wandekar <vishalwandekar@gmail.com>
2025-12-10 23:13:56 +0530

    created MainContainer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Body from './components/Body';
 import Head from './components/Head';
+import Sidebar from './components/Sidebar';
 import './index.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <h1 className="text-3xl font bold">Namaste react</h1>
       <Head />
       <Body />
+      <Sidebar />
     </div>
   );
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Sidebar from "./Sidebar";
+import MainContainer from "./MainContainer";
 
 const Body = () =>{
-    return <div>Body</div>
+    return <div>
+        <Sidebar />
+        <MainContainer/>
+    </div>
 };
 
 export default Body;
