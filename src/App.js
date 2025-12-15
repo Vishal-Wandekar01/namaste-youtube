14c2df580d7363b9c47457f8ed5c71f2cc128a6d
Vishal Wandekar <vishalwandekar@gmail.com>
2025-12-15 12:46:34 +0530

    Provided our store to app,  and added the functionality using dispatcher and selector

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 
+import { Provider } from 'react-redux';
 import './App.css';
 import Body from './components/Body';
 import Head from './components/Head';
 import './index.css';
+import store from './utils/store';
 
 function App() {
   return (
+    <Provider store={store}>
     <div >
     
       <Head />
       <Body />
      
     </div>
+    </Provider>
   );
 }
 
