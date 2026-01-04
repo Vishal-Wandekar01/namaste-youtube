eea29cc7e53e5ee954f72d5a202d57d6f78ece58
Vishal Wandekar <vishalwandekar@gmail.com>
2026-01-04 21:53:35 +0530

    increased the vodeo count from API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const GOOGLE_API_KAY = "AIzaSyA0GWzNVO6s67cZxgnWiXna60YQ-2MEXX0";
 
 export const YOUTUBE_VIDEO_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" + GOOGLE_API_KAY;
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" + GOOGLE_API_KAY;
 
 
