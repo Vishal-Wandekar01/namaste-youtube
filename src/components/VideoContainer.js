eb97a599d5374041dd8b2a4a1f011403fb0aa3aa
Vishal Wandekar <vishalwandekar@gmail.com>
2026-01-04 21:27:51 +0530

    Use the data feom the API and we are using state for managing that data, Created videoCard as well

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,17 +1,25 @@
-import { useEffect } from "react";
-import {YOUTUBE_VIDEO_API} from "../utils/constants";
+import { useEffect, useState } from "react";
+import { YOUTUBE_VIDEO_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+
   useEffect(() => {
     getVideos();
   }, []);
 
-  const getVideos = async ()=>{
+  const getVideos = async () => {
     const data = await fetch(YOUTUBE_VIDEO_API);
     const json = await data.json();
-    console.log(json);
-  }
-  return <div>VideoContainer</div>;
+    //console.log(json.items);
+    setVideos(json.items);
+  };
+  return (
+    <div>
+      <VideoCard info={videos[0]} />
+    </div>
+  );
 };
 
 export default VideoContainer;
