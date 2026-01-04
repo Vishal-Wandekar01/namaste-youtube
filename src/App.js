68a1ad38969d50721a7f40bc0777b5a3f22eec64
Vishal Wandekar <vishalwandekar@gmail.com>
2026-01-04 23:07:36 +0530

    implemeted rounting, created outlet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
+import { Provider } from "react-redux";
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+import "./index.css";
+import store from "./utils/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
 
-import { Provider } from 'react-redux';
-import './App.css';
-import Body from './components/Body';
-import Head from './components/Head';
-import './index.css';
-import store from './utils/store';
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
-    <div >
-    
-      <Head />
-      <Body />
-     
-    </div>
+      <div>
+        <Head />
+        <RouterProvider router={appRouter} />
+      </div>
     </Provider>
   );
 }
 
 export default App;
-
 
 /**
  * Head
