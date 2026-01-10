7a7b81a74448e4a5a26e125d3cee44255560bb72
Vishal Wandekar <vishalwandekar@gmail.com>
2026-01-10 23:11:29 +0530

    Implemented Comments section

--- src/utils/searchSlice.js
+++ src/utils/searchSlice.js
@@ -13,3 +13,6 @@ const searchSlice = createSlice({
 export const { cacheResults } = searchSlice.actions;
 
 export default searchSlice.reducer;
+
+//LRU cache-least recently used-we can restrict our cache to only 100 keys..start removing from the top
+
