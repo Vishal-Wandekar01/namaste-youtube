376c5b3334c819894bef74db0ebd3569ed5bd54d
Vishal Wandekar <vishalwandekar@gmail.com>
2026-01-15 19:25:59 +0530

    added the limit for live chat, After reaching perticuler threshold it removes the chat

--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { OFFSET_LIVE_CHAT } from "./constants";
 
 const chatSlice = createSlice({
   name: "chat",
@@ -7,6 +8,8 @@ const chatSlice = createSlice({
   },
   reducers: {
     addMessage: (state, action) => {
+        if(state.message.length)
+            state.message.splice(OFFSET_LIVE_CHAT,1);
       state.message.unshift(action.payload);
     },
   },
